78ef4a3c18c6bc7c1683f446f04b066bf4e47ee6
Abdullah Khan <aabdullahkhan4@gmail.com>
2025-08-22 22:24:04 +0500

    Changes made

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> @/components/ChatPage\":{\"id\":\"app/page.tsx -> @/components/ChatPage\",\"files\":[\"static/chunks/_app-pages-browser_components_ChatPage_tsx.js\"]}}"
